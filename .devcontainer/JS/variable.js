11b99c4dda151532589ac41fa9b8d7bbe52506d0
dhairya-gohil-1004 <dhairyagohil1004@outlook.com>
2025-03-21 17:44:14 +0000

    commit done

---
+++ .devcontainer/JS/variable.js
@@ -0,0 +1,15 @@
+const accountId = "1004"
+let accountEmail = "dhairya1004@gohil.com"
+var accountPassword = "1234"
+accountCity = "Vadodara"
+
+accountEmail = "dhairya@rajput.com"
+accountPassword = "1004"
+accountCity = "Bengaluru"
+
+//avoid using of var
+
+console.log(accountEmail)
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
